af0b4892c62247c8ad582924aba67d8ff8b22a72
Eric Carlton <eric121492@gmail.com>
2019-06-26 23:30:12 -0400

    refactored to use getopt_long to support long arguments

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <stdio.h> // printf, fprintf, scanf
+#include <stdlib.h> // system, abort, strtol
+#include <errno.h> // errno, ERANGE
+#include <getopt.h> // getopt_long
+#include <string.h> // strcpy, strlen
+#include <stdbool.h> // true / false
 
 #define MAX_STRING_ARG_LENGTH 20
 #define MAX_END_NUMBER_DIGITS 10
@@ -15,37 +14,48 @@ struct FizzBuzzConfig {
 };
 
 void processOptions(int argc, char **argv, struct FizzBuzzConfig *config) {
+	struct option longOpts[] =
+	{
+		{"fizz",  required_argument, 0, 'f'},
+		{"buzz",    required_argument, 0, 'b'},
+		{0, 0, 0, 0}
+	};
+
 	int opt;
 
-	while ((opt = getopt(argc, argv, "f:b:")) != -1) {
-	    switch (opt)
-	      {
-		      case 'f':
-		      	if(strlen(optarg) > MAX_STRING_ARG_LENGTH) {
-		      		fprintf(stderr, "Option -f has a max length of %d characters\n", MAX_STRING_ARG_LENGTH);
-		      		config->valid = false;
-		      	} else {
-		      		strcpy(config->fizz, optarg);
-		      	}
-		        break;
-		      case 'b':
-		        if(strlen(optarg) > MAX_STRING_ARG_LENGTH) {
-		      		fprintf(stderr, "Option -b has a max length of %d characters\n", MAX_STRING_ARG_LENGTH);
-		      		config->valid = false;
-		      	} else {
-		      		strcpy(config->buzz, optarg);
-		      	}
-		        break;
-		      case '?':
-		        if (optopt == 'f' || optopt == 'b') {
-		          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-		   		  config->valid = false;
-		        }
-		        break;
-		      default:
-		        abort();
-	      }
-  	}
+	while (true) {
+		opt = getopt_long (argc, argv, "f:b:", longOpts, NULL);
+
+		// end of options, break out of loop
+		if (opt == -1) break;
+
+		switch (opt) {
+		case 'f':
+			if (strlen(optarg) > MAX_STRING_ARG_LENGTH) {
+				fprintf(stderr, "Option -f has a max length of %d characters\n", MAX_STRING_ARG_LENGTH);
+				config->valid = false;
+			} else {
+				strcpy(config->fizz, optarg);
+			}
+			break;
+		case 'b':
+			if (strlen(optarg) > MAX_STRING_ARG_LENGTH) {
+				fprintf(stderr, "Option -b has a max length of %d characters\n", MAX_STRING_ARG_LENGTH);
+				config->valid = false;
+			} else {
+				strcpy(config->buzz, optarg);
+			}
+			break;
+		case '?':
+			if (optopt == 'f' || optopt == 'b') {
+				fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				config->valid = false;
+			}
+			break;
+		default:
+			abort();
+		}
+	}
 }
 
 void printIntro(struct FizzBuzzConfig config) {
@@ -61,26 +71,26 @@ int getStopNumberFromUser() {
 	char unprocessedInput[MAX_END_NUMBER_DIGITS];
 
 	printf("How high should I count? (must be greater than 0 and have fewer than %d digits; any whitespace constitutes end of input): ", MAX_END_NUMBER_DIGITS);
-    // TODO: prevent inputs with greater length than MAX_END_NUMBER_DIGITS
-    scanf("%s", unprocessedInput);
+	// TODO: prevent inputs with greater length than MAX_END_NUMBER_DIGITS
+	scanf("%s", unprocessedInput);
 
-    int result = strtol(unprocessedInput, NULL, 10);
+	int result = strtol(unprocessedInput, NULL, 10);
 
-    if(errno == ERANGE) {
-    	printf("range error, got %d\n", result);
-    	return -1;
-    }
+	if (errno == ERANGE) {
+		printf("range error, got %d\n", result);
+		return -1;
+	}
 
-    return result;
+	return result;
 }
 
 void playFizzbuzz(int start, int end, struct FizzBuzzConfig config) {
-	for(int i = start; i <= end; i++) {
+	for (int i = start; i <= end; i++) {
 		printf("%d: ", i);
-		if(i % 3 == 0) {
+		if (i % 3 == 0) {
 			printf("%s", config.fizz);
 		}
-		if(i % 5 == 0) {
+		if (i % 5 == 0) {
 			printf("%s", config.buzz);
 		}
 		printf("\n");
@@ -97,12 +107,12 @@ int main(int argc, char **argv) {
 	struct FizzBuzzConfig config = {"FIZZ", "BUZZ", true};
 	processOptions(argc, argv, &config);
 
-	if(config.valid) {
+	if (config.valid) {
 		printIntro(config);
 
 		int end = getStopNumberFromUser();
 
-		if(end > 0) {
+		if (end > 0) {
 			playFizzbuzz(1, end, config);
 		} else {
 			handleInputError();
